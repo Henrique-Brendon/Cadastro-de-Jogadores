7077771529fd00c3e04be02a3255ba01d1f7adc9
henrique brendon <henriquebrendonti26@gmail.com>
2025-02-20 19:07:51 -0300

    Consome API externa para buscar codinomes da Liga da Justiça em XML e adiciona dependência jackson-dataformat-xml

--- src/main/java/com/henrique_brendon/cadastro_jogadores/repositories/LigaDaJustiçaRepository.java
+++ src/main/java/com/henrique_brendon/cadastro_jogadores/repositories/LigaDaJustiçaRepository.java
@@ -2,7 +2,26 @@ package com.henrique_brendon.cadastro_jogadores.repositories;
 
 import java.util.List;
 
+import org.springframework.web.client.RestClient;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.henrique_brendon.cadastro_jogadores.dtos.LigaDaJusticaDTO;
+import com.henrique_brendon.cadastro_jogadores.entities.enums.GrupoCodinome;
+
 public class LigaDaJustiçaRepository implements CodinomeRepository {
 
+    @Override
+    public List<String> buscarCodinomes() throws Exception {
+        var codinomes = RestClient.builder()
+            .baseUrl(GrupoCodinome.LIGA_DA_JUSTICA.getUri())
+            .build()
+            .get()
+            .retrieve()
+            .body(String.class);
+
+        var xmlMapper = new XmlMapper();
+        var ligaDaJustica = xmlMapper.readValue(codinomes, LigaDaJusticaDTO.class);
+        return ligaDaJustica.getCodinomes();
+    }
 
 } 
