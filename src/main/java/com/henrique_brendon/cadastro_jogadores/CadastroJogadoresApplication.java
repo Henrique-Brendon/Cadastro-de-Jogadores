e431b6531c247b00e10a2b25f560a6f9d46480b7
henrique brendon <henriquebrendonti26@gmail.com>
2025-02-20 16:08:59 -0300

    Commit inicial

---
+++ src/main/java/com/henrique_brendon/cadastro_jogadores/CadastroJogadoresApplication.java
@@ -0,0 +1,13 @@
+package com.henrique_brendon.cadastro_jogadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroJogadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroJogadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henrique_brendon/cadastro_jogadores/CadastroJogadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique_brendon.cadastro_jogadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroJogadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
